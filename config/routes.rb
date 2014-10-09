d37548f7288420b7245fa5969513b8c474311843
mr-kremez <mr.kremez@gmail.com>
2014-10-09 16:16:37 +0300

    Scaffold book model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :books
+
   get 'persons/profile'
 
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141009115102) do
+ActiveRecord::Schema.define(version: 20141009130631) do
+
+  create_table "books", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
