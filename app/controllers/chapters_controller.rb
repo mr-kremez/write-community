0e218b901a318e310e906a17c32769d1ca14a865
mr-kremez <mr.kremez@gmail.com>
2014-10-11 18:28:34 +0300

    Make pagination for chapters show

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -4,12 +4,26 @@ class ChaptersController < ApplicationController
   before_action :set_book
 
   def index
-    @chapters = @book.chapters
-    @chapter = @chapters.first
     respond_with(@chapter)
   end
 
   def show
+    @chapter_id = params[:id].to_i
+    @first_chapter_id_of_the_book = @book.chapters.select(:id).first[:id].to_i
+    @last_chapter_id_of_the_book = @book.chapters.select(:id).last[:id].to_i
+    if @chapter_id < @last_chapter_id_of_the_book
+      next_chapter = @chapter_id + 1
+    else
+      next_chapter = @chapter_id
+    end
+    if @chapter_id > @first_chapter_id_of_the_book
+      prev_chapter = @chapter_id - 1
+    else
+      prev_chapter = @chapter_id
+    end
+
+    @prev_chapter = Chapter.find(prev_chapter)
+    @next_chapter = Chapter.find(next_chapter)
     respond_with(@chapter)
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,12 +16,12 @@ Category.create(:name => "IT")
 Category.create(:name => "Society")
 
 50.times do |i|
-	Book.create(:name => "Book #{i}", :description => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel tincidunt metus, eu lobortis odio. Duis ipsum lorem, fringilla vitae euismod vitae, eleifend ultricies velit. Nam maximus nunc eu pharetra facilisis. In fringilla felis volutpat ex lacinia dignissim. Quisque nec metus eu justo mattis molestie. Suspendisse egestas nunc nec ipsum aliquet laoreet. Quisque dignissim dui quis lectus fermentum, non varius justo cursus. Nam vel lorem sed lectus interdum auctor. Mauris porttitor neque purus, in lacinia enim ultricies eget. Curabitur in lacus est. Vestibulum eu fringilla nulla.", 
+	Book.create(:name => "Book #{i + 1}", :description => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel tincidunt metus, eu lobortis odio. Duis ipsum lorem, fringilla vitae euismod vitae, eleifend ultricies velit. Nam maximus nunc eu pharetra facilisis. In fringilla felis volutpat ex lacinia dignissim. Quisque nec metus eu justo mattis molestie. Suspendisse egestas nunc nec ipsum aliquet laoreet. Quisque dignissim dui quis lectus fermentum, non varius justo cursus. Nam vel lorem sed lectus interdum auctor. Mauris porttitor neque purus, in lacinia enim ultricies eget. Curabitur in lacus est. Vestibulum eu fringilla nulla.", 
 		:category_id => rand(1..4), :user_id  => rand(1..2))
 end
 
 10.times do |i|
-	Chapter.create(:name => "Chapter #{i}", :content => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et sollicitudin ante. Duis faucibus diam sollicitudin, porta mi vitae, viverra nibh. Mauris sed porttitor felis, a malesuada arcu. Ut vitae lectus vel mi tincidunt tincidunt. Cras id diam egestas orci hendrerit sagittis. Fusce ultrices consequat quam, at malesuada leo viverra vel. Ut pharetra id lectus nec consequat. Morbi sed eleifend dui. Etiam faucibus molestie turpis, nec facilisis leo maximus eu. Donec maximus ac risus ut dictum. Suspendisse sed dapibus felis, non laoreet nunc.
+	Chapter.create(:name => "Chapter #{i + 1}", :content => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et sollicitudin ante. Duis faucibus diam sollicitudin, porta mi vitae, viverra nibh. Mauris sed porttitor felis, a malesuada arcu. Ut vitae lectus vel mi tincidunt tincidunt. Cras id diam egestas orci hendrerit sagittis. Fusce ultrices consequat quam, at malesuada leo viverra vel. Ut pharetra id lectus nec consequat. Morbi sed eleifend dui. Etiam faucibus molestie turpis, nec facilisis leo maximus eu. Donec maximus ac risus ut dictum. Suspendisse sed dapibus felis, non laoreet nunc.
 
 Integer ac feugiat urna. Praesent vitae sapien lacus. Praesent dapibus lorem velit, at rutrum augue sodales at. Suspendisse suscipit libero ut urna maximus, sit amet aliquam nisl vulputate. Cras quis placerat neque. Vestibulum pellentesque rhoncus purus, vitae pellentesque lectus. Nulla facilisi. Pellentesque ac elit neque. Donec nisl magna, consectetur sollicitudin quam ac, dapibus egestas odio. Nunc semper felis nec diam consectetur mattis.
 
@@ -33,4 +33,18 @@ Phasellus laoreet lacus sed lacus venenatis, vehicula tincidunt est porttitor. V
 
 ",
 		:book_id => 1)
+end
+10.times do |i|
+	Chapter.create(:name => "Chapter #{i + 1}", :content => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et sollicitudin ante. Duis faucibus diam sollicitudin, porta mi vitae, viverra nibh. Mauris sed porttitor felis, a malesuada arcu. Ut vitae lectus vel mi tincidunt tincidunt. Cras id diam egestas orci hendrerit sagittis. Fusce ultrices consequat quam, at malesuada leo viverra vel. Ut pharetra id lectus nec consequat. Morbi sed eleifend dui. Etiam faucibus molestie turpis, nec facilisis leo maximus eu. Donec maximus ac risus ut dictum. Suspendisse sed dapibus felis, non laoreet nunc.
+
+Integer ac feugiat urna. Praesent vitae sapien lacus. Praesent dapibus lorem velit, at rutrum augue sodales at. Suspendisse suscipit libero ut urna maximus, sit amet aliquam nisl vulputate. Cras quis placerat neque. Vestibulum pellentesque rhoncus purus, vitae pellentesque lectus. Nulla facilisi. Pellentesque ac elit neque. Donec nisl magna, consectetur sollicitudin quam ac, dapibus egestas odio. Nunc semper felis nec diam consectetur mattis.
+
+Aliquam feugiat euismod leo, et hendrerit urna scelerisque non. Praesent pretium porta mauris quis dapibus. Fusce nec augue nisi. Nulla ullamcorper ultrices auctor. Mauris non arcu eleifend, cursus dui vitae, aliquet odio. Ut tempus non eros eu eleifend. Donec ac dolor vestibulum, rutrum ex et, malesuada quam. Donec luctus dictum est, et consequat dolor feugiat sed. Donec pretium sed velit sed aliquet.
+
+Praesent tempor ligula in eros cursus, vel porttitor magna fringilla. Nunc tellus nulla, rhoncus vitae eros non, fringilla sagittis diam. Sed eu odio a arcu pulvinar consectetur eget ut sem. Nullam in mi imperdiet, blandit enim ut, egestas libero. Quisque ultrices mauris nec tellus tempus, a posuere odio tempus. Nunc semper vitae orci in venenatis. Ut dignissim nibh non justo feugiat, nec dictum turpis rhoncus. Suspendisse potenti. Donec vitae est at elit suscipit mollis. Cras sed pharetra nisl, ut finibus justo. Donec vulputate augue eu ipsum tristique congue. Aliquam fringilla ultrices nulla, id blandit mi eleifend eget.
+
+Phasellus laoreet lacus sed lacus venenatis, vehicula tincidunt est porttitor. Vestibulum dignissim ante et mauris tincidunt, sed pharetra libero imperdiet. Integer euismod semper dui. Nam sollicitudin lectus dolor, at tincidunt diam ullamcorper sit amet. Maecenas justo massa, dictum sed tellus vitae, tempus accumsan massa. Mauris egestas ac mauris in ultricies. In rhoncus ante sit amet mattis interdum. Mauris fermentum, odio id maximus dictum, leo tortor sagittis dui, quis lacinia elit ex nec mauris. Proin fermentum egestas leo a elementum.
+
+",
+		:book_id => 2)
 end
