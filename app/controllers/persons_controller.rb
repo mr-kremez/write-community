bb2cc0d3cef6b7b77fb55cb63f57e9060acaa1b9
mr-kremez <mr.kremez@gmail.com>
2014-10-12 14:16:43 +0300

    Add anotation to profile

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -1,7 +1,7 @@
 class PersonsController < ApplicationController
-	#before_action :authenticate_member!
-	require 'book'
+	before_action :authenticate_user!
   	def profile
   		@books = Book.where(:user_id => current_user.id).page(params[:page]).per(params[:per])
+  		@anotations = User.find(current_user).anotations
   	end
 end
